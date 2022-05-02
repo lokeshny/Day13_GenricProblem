8ac9839d584d55bc231089d55e88ff31f83511f5
lokeshny <lokeshny97@gmail.com>
2022-05-02 15:18:34 +0530

    Added: Given three intiger Max Value

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.BridgeLab.Genrics;
+
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to check max of three variables");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter values of a, b and c: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        //testMaximum method called
+        TestMaximum.testMaximum(a,b,c);
+    }
+}
