b578430c9d5881dd016f42729448373c0c7d826a
lokeshny <lokeshny97@gmail.com>
2022-05-02 15:39:25 +0530

    Added: Refactor to creat Genric class to take 3 variable of generic type

--- Main.java
+++ Main.java
@@ -7,13 +7,25 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("Welcome to check max of three variables");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter values of String 1: ");
+        //Taking String input from the user
+        System.out.println("Enter values of String 1, String 2, String 3: ");
         String string1 = sc.nextLine();
-        System.out.println("Enter values of String 2: ");
         String string2 = sc.nextLine();
-        System.out.println("Enter values of String 3: ");
         String string3 = sc.nextLine();
+        //Taking integer values from the user
+        System.out.println("Enter Integer values of a, b, c: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        //Taking float values from the user
+        System.out.println("Enter Float values of x, y, z: ");
+        float x = sc.nextFloat();
+        float y = sc.nextFloat();
+        float z = sc.nextFloat();
+
         //testMaximum method called
-        TestMaximum.testMaximum(string1, string2, string3);
-        }
-        }
+        TestMaximum.<String>testMaximum(string1, string2, string3);
+        TestMaximum.<Integer>testMaximum(a,b,c);
+        TestMaximum.<Float>testMaximum(x,y,z);
+    }
+}
--- TestMaximum.java
+++ TestMaximum.java
@@ -2,39 +2,43 @@
 /*
  * The Generics program implements Test cases that simply
  * finds Maximum of 3 variables using Generics.
- * Master - Given 3 Float. find the maximum
  * @author: Lokesh
- * @date: 01-04-2022
+ * @date: 01-05-2022
  */
 
 package com.BridgeLab.Genrics;
-public class TestMaximum {
-    String x, y, z;
 
-    // constructor
-    public TestMaximum(String x, String y, String z) {
+public class TestMaximum <T extends Comparable<T>>{
+    private T x, y, z;
+
+    // constructor test maximum
+    public TestMaximum(T x, T y, T z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
     /*
-     * testMaximum method created for checking max of three Strings
+     * testMaximum method extends comparable T to Extend the max method to take more than three parameters
      */
-    public static String testMaximum(String x, String y, String z) {
-        // Initializing x as greater
-        String max = x;
-        if (y.compareTo(max) > 0)
-            // as of now y is greater
-            max = y;
-
-        if (z.compareTo(max) > 0)
-            // now z is greater
-            max = z;
-        printMax(x,y,z,max);
+    public static<T extends Comparable<T>> T testMaximum(T...values){
+        T max = values[0];
+        for(T element : values){
+            if(element.compareTo(max)>0){
+                max = element;
+            }
+        }
+        printMax(max);
         return max;
-
     }
+
+    /*
+     *  printMax method created for displaying maximum values for given variables.
+     */
+    public static <T> void printMax(T max) {
+        System.out.println("The maximum values is:\t" +max);
+    }
+
 
     /*
      *  printMax method created for displaying maximum values from the given 3 variables.
